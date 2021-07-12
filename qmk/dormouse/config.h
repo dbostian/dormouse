0f4d2072619c6e7d69c9a7d21cb2fab6986debcc
Dan Bostian <dbostian@gmail.com>
2021-07-11 21:08:35 -0700

    Mouse Keys defaults

--- qmk/dormouse/config.h
+++ qmk/dormouse/config.h
@@ -55,4 +55,11 @@
 #define RGBLIGHT_VAL_STEP 8
 #endif
 
+/* Mouse Keys */
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+
 #endif
